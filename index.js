79e2f3fe8a523babdd0b09c808bdcaa1c8ae5525
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-10-26 13:50:55 -0400

    csv data

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 //This is the CSV data from the previous assignment 308.3.1
-const csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
+const csvData = `ID,Name,Occupation,Age
+42,Bruce,Knight,41
+57,Bob,Fry Cook,19
+63,Blaine,Quiz Master,58
+98,Bill,Doctor’s Assistant,26`;
+console.log(csvData)
+
+//Part 1 Refactoring/Restructuring the CSV Data from 308.3.1
+const rows = csvData.split('\n');
+
