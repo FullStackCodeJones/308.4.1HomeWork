bce658d5bcc628c9f4a7b5859907dde611bf6a44
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-10-26 18:35:40 -0400

    What do you mean DONT CRY????

--- index.js
+++ index.js
@@ -41,15 +41,43 @@ let headers = [];
 
 randi.forEach((string) => {
     //check if first line
-    if (string == randi[0] ){
+    if (string == randi[0]) {
         console.log(string)
         headers = string.toLowerCase().split(',')
         console.log(headers)
     }
     data.push(string.split(','));
 })
-//console.log(data);
+//Sighhhhhhhhh My Head and eyes hurt 🥺I'm learning though.. kind of😊
+////Part 3???=================================================
+
+let people = [
+    { id: "42", name: "Bruce", occupation: "Knight", age: "41" },
+    { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
+    { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
+    { id: "99", name: "Brett", occupation: "Developer", age: "33" }
+];
+
+people.pop();
 
 
+people.splice(1, 0, { id: "48", name: "Barry", occupation: "Runner", age: "25" });
+
+
+people.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
+
+
+console.log(people);
+
+
+let totalAge = 0;
+for (let i = 0; i < people.length; i++) {
+    totalAge += parseInt(people[i].age);
+}
+let averageAge = totalAge / people.length;
+
+console.log("Average Age:", averageAge);
+
+//============================💃🏽=========💃🏽==========================================
 
 
