2913b4df900fcbcbfa99214d1c27ca5214c09ee7
FullStackCodeJones <fullstackcodejones@gmail.com>
2024-10-26 13:11:45 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-//CSV Data
-const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
-
-//Parse CSV data into a 2D array
-const rows = csvData.split('\n');
-const columnCount = rows[0].split(',').length;
-const tableData = rows.map(row => row.split(','));
-
-//Convert each row into an object with keys from headers
-const headers = tableData[0];  // ["ID", "Name", "Occupation", "Age"]
-const objects = tableData.slice(1).map(row => {
-    const obj = {};
-    headers.forEach((header, index) => {
-        obj[header.toLowerCase()] = row[index]; // lowercase keys for consistency
-    });
-    return obj;
-});
-console.log("Array of objects:", objects);
+//This is the CSV data from the previous assignment 308.3.1
+const csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
